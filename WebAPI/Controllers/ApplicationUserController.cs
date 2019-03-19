3b08d415a1eb2ac6e2ebc81b69f015a46ff17359
tombradev <tombradev@gmail.com>
2019-03-19 14:08:03 +0700

    Successful Build for POST Method
    Still using the default Validation data

---
+++ WebAPI/Controllers/ApplicationUserController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUserController : ControllerBase
+    {
+        private UserManager<ApplicationUser> _UserManager;
+        private SignInManager<ApplicationUser> _SignInManager;
+
+        public ApplicationUserController(UserManager<ApplicationUser> UserManager, SignInManager<ApplicationUser> SignInManager)
+        {
+            _UserManager = UserManager;
+            _SignInManager = SignInManager;
+        }
+
+        /*
+         * The web api method
+         */
+
+        [HttpPost]
+        [Route("Register")]
+        //link : //POST : /api/ApplicationUser/Register
+
+        public async Task<Object> PostApplicationUser(ApplicationUserModel model)
+        {
+            var applicationUser = new ApplicationUser()
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+                FullName = model.FullName
+            };
+
+            try
+            {
+                var result = await _UserManager.CreateAsync(applicationUser, model.Password);
+                return Ok(result);
+            }
+            catch (Exception On)
+            {
+                throw On;
+            }
+        }
+    }
+}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -33,6 +33,7 @@ namespace WebAPI
 
             services.AddDefaultIdentity<ApplicationUser>()
                 .AddEntityFrameworkStores<AuthenticationContext>(); // This add identity core with EF functionality
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
